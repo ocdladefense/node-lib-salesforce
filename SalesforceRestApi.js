863fb163d27def38774c596a0dd41a2cb60b8a58
eila <dershameila@gmail.com>
2025-09-15 12:45:58 -0700

    Modified SalesforceRestApi.js.

--- SalesforceRestApi.js
+++ SalesforceRestApi.js
@@ -1,21 +1,29 @@
 import HttpClient from '@ocdla/lib-http/HttpClient.js';
 
+
 /**
  * @module SalesforceRestApi
- * @classdesc This module is to be used to interface with the Salesforce API using a RESTful architecture. 
+ * @classdesc This module is to be used to interface with the Salesforce API using a RESTful architecture.
  */
 export default class SalesforceRestApi extends HttpClient {
     instanceUrl;
 
+
     accessToken;
+
 
     path;
 
+
     method;
+
 
     headers;
 
+
     body;
+
+
 
 
     /**
@@ -29,9 +37,10 @@ export default class SalesforceRestApi extends HttpClient {
      * */
     static BASE_URL = '/services/data/' + SalesforceRestApi.API_VERSION + '/';
 
-    /** 
+
+    /**
      * Represents this API that interfaces with the Salesforce API.
-     * @constructor 
+     * @constructor
      * @param {string} instanceUrl - The
      * @param {string} accessToken - The
     */
@@ -45,7 +54,8 @@ export default class SalesforceRestApi extends HttpClient {
         this.headers.append('Content-Type', 'application/json');
     }
 
-    /** 
+
+    /**
      * @param {string} queryString - The SQL query.
      * @returns {object}
      * @example
@@ -53,13 +63,66 @@ export default class SalesforceRestApi extends HttpClient {
     */
     async query(queryString) {
 
+
         this.method = 'GET';
         this.path = SalesforceRestApi.BASE_URL + 'query?q=' + queryString;
+
+
+        let resp = await this.send();
+
+
+        // if (["PATCH", "DELETE"].includes(this.method)) {
+        //     return resp.then(resp => resp.ok);
+        // }
+
+
+        return await resp.json();
+        // .then((json) => {
+        //     if (json.errorCode != null) {
+        //         console.log(json.errorCode);
+        //     }
+        //     return json;
+        // })
+        // .catch((err) => {
+        //     console.error('Error:', err);
+        // });
+
+
+    }
+
+
+    /**
+    * @param {string} resourceId - The SQL query.
+    * @returns {object}
+    * @example
+    * "SELECT Name,Id FROM Object_Name"
+    */
+    async access(resourceId) {
+
+
+        this.method = 'GET';
+        this.path = "/services/apexrest/api/media/access/" + resourceId;
+
 
         return await this.send();
     }
 
-    /** 
+
+    /**
+    * @param {string} resourceId - The SQL query.
+    * @returns {object}
+    */
+    async purchase(resourceId) {
+
+        this.method = 'POST';
+        this.path = "/services/apexrest/api/media/" + resourceId + "/purchase";
+
+
+        return await this.send();
+    }
+
+
+    /**
      * @param {string} objectName
      * @param {object} record
      * @returns {object}
@@ -69,10 +132,12 @@ export default class SalesforceRestApi extends HttpClient {
         this.path = SalesforceRestApi.BASE_URL + 'sobjects/' + objectName;
         this.body = JSON.stringify(record);
 
+
         return this.send();
     }
 
-    /** 
+
+    /**
      * @param {string} objectName
      * @param {object} record
      * @returns {object}
@@ -83,11 +148,14 @@ export default class SalesforceRestApi extends HttpClient {
         delete record.Id;
         this.body = JSON.stringify(record);
 
+
         return this.send();
     }
 
 
-    /** 
+
+
+    /**
      * @param {string} objectName
      * @param {object} record
      * @returns {object}
@@ -97,24 +165,30 @@ export default class SalesforceRestApi extends HttpClient {
         this.method = 'PATCH';
         let basePath = SalesforceRestApi.BASE_URL + 'sobjects/' + objectName;
 
+
         if (idField == "Id") {
             basePath += `/${idValue}`;
         } else {
             basePath += `/${idField}/${encodeURIComponent(idValue)}`;
         }
 
+
         this.path = basePath;
+
 
         // Always remove the id field (or external id field) from the payload.
         // Otherwise = 400 Bad Request.
         delete record[idField];
 
+
         this.body = JSON.stringify(record);
+
 
         return this.send();
     }
 
-    /** 
+
+    /**
      * @param {string} objectName
      * @param {object} record
      * @returns {object}
@@ -123,10 +197,12 @@ export default class SalesforceRestApi extends HttpClient {
         this.method = 'DELETE';
         this.path = SalesforceRestApi.BASE_URL + 'sobjects/' + objectName + `/${record}`;
 
+
         return this.send();
     }
 
-    /** 
+
+    /**
      * @returns {object}
     */
     send() {
@@ -135,28 +211,24 @@ export default class SalesforceRestApi extends HttpClient {
             headers: this.headers
         };
 
+
         if (["GET", "DELETE"].includes(this.method) == false) {
             config.body = this.body;
         }
 
+
         const req = new Request(this.instanceUrl + this.path, config);
 
-        let resp = super.send(req);
 
-        if (["PATCH", "DELETE"].includes(this.method)) {
-            return resp.then(resp => resp.ok);
-        }
-        else {
-            return resp.then(resp => resp.json())
-                .then((json) => {
-                    if (json.errorCode != null) {
-                        console.log(json.errorCode);
-                    }
-                    return json;
-                })
-                .catch((err) => {
-                    console.error('Error:', err);
-                });
-        }
+        return super.send(req);
+
+
+
+
+
+
     }
 }
+
+
+
