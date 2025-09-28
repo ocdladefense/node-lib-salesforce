587479181ae7087c95f4237ce49a18213762235a
eila <dershameila@gmail.com>
2025-09-28 13:44:53 -0700

    Added CookieUtils file.

---
+++ CookieUtils.js
@@ -0,0 +1,16 @@
+export function getCookie(name) {
+    const cookieName = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cookieName) === 0) {
+            return c.substring(cookieName.length, c.length);
+        }
+    }
+    return "";
+}
+
